6759165b7ee5f1638817b025f2d53aa58a9cd6d3
Olivier Pinon <op@impero.com>
2022-12-13 22:49:34 +0100

    Better day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,94 +1,53 @@
+use pathfinding::prelude::{bfs, Matrix};
+
 struct Heightmap {
-    map: Vec<u8>,
-    width: i32,
-    height: i32,
-    start: i32,
-    end: i32,
+    map: Matrix<u8>,
+    start: (usize, usize),
+    end: (usize, usize),
 }
 
 #[aoc_generator(day12)]
 fn parse(input: &str) -> Heightmap {
-    let mut width = 0;
-    let mut height = 0;
-    let mut map = Vec::new();
-    let mut start = 0;
-    let mut end = 0;
-    let mut idx = 0;
-    for l in input.lines() {
-        width = l.len();
-        height += 1;
-        for c in l.chars() {
-            let elevation = match c {
-                'S' => {
-                    start = idx;
-                    0
-                }
-                'E' => {
-                    end = idx;
-                    25
-                }
-                x => x as u8 - 'a' as u8,
-            };
-            map.push(elevation);
-            idx += 1;
-        }
-    }
-    Heightmap {
-        map,
-        width: width as i32,
-        height,
-        start,
-        end,
-    }
-}
-
-fn solve_for_start(input: &Heightmap, start: i32) -> Option<usize> {
-    let start_x = start % input.width;
-    let start_y = start / input.width;
-    let end_x = input.end % input.width;
-    let end_y = input.end / input.width;
-    let (path, _) = pathfinding::directed::astar::astar(
-        &(start_x, start_y),
-        |&(x, y)| {
-            let elevation = input.map[(y * input.width + x) as usize];
-            let neighbors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-                .into_iter()
-                .filter(|&(x, y)| {
-                    x >= 0
-                        && y >= 0
-                        && x < input.width
-                        && y < input.height
-                        && elevation + 1 >= input.map[(y * input.width + x) as usize]
-                })
-                .map(|pos| (pos, 1))
-                .collect::<Vec<_>>();
-            neighbors
-        },
-        |(x, y)| (end_x - x).abs() * (end_y - y).abs(),
-        |&pos| pos == (end_x, end_y),
-    )?;
-    Some(path.len() - 1)
+    let mut map = Matrix::from_rows(input.lines().map(str::bytes)).expect("Failed to build map");
+    // I'm very sad we can't find those in one pass (when building the Matrix),
+    // but I couldn't find how!
+    let start = map.indices().find(|p| map[*p] == b'S').unwrap();
+    let end = map.indices().find(|p| map[*p] == b'E').unwrap();
+    map[start] = b'a';
+    map[end] = b'z';
+    Heightmap { map, start, end }
 }
 
 #[aoc(day12, part1)]
 fn part1(input: &Heightmap) -> usize {
-    solve_for_start(input, input.start).unwrap()
+    let path = bfs(
+        &input.start,
+        |&p| {
+            input
+                .map
+                .neighbours(p, false)
+                .filter(move |n| input.map[p] + 1 >= input.map[*n])
+        },
+        |&p| p == input.end,
+    )
+    .expect("Failed to find path");
+    path.len() - 1
 }
 
 #[aoc(day12, part2)]
 fn part2(input: &Heightmap) -> usize {
-    input
-        .map
-        .iter()
-        .enumerate()
-        .filter_map(|(idx, elev)| {
-            if *elev == 0 {
-                return solve_for_start(input, idx as i32);
-            }
-            None
-        })
-        .min()
-        .unwrap()
+    let path = bfs(
+        &input.end,
+        |&p| {
+            input
+                .map
+                .neighbours(p, false)
+                .filter(move |n| input.map[p] <= input.map[*n] + 1)
+        },
+        |&p| input.map[p] == b'a',
+    )
+    .expect("Failed to find path");
+    path.len() - 1
 }
 
 #[cfg(test)]
